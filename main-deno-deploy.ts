050d4dade7bb5a3da33924b5e057af2b0960b08d
Tomas Kubat <tomas.kubat@gmail.com>
2021-12-07 22:45:53 +0100

    Deno Deploy bootstrap

--- main-deno-deploy.ts
+++ main-deno-deploy.ts
@@ -1,4 +1,6 @@
 import { app } from "./bootstrap.ts";
 
 // Deno Deploy
-addEventListener("fetch", app.fetchEventHandler());
+//addEventListener("fetch", app.fetchEventHandler());
+console.log('Running on port 8080');
+await app.listen({ port: 8080 });
